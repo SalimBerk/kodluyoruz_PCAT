324950c3592b0ae14be8cd8cf205d432970f6d8a
salim <salimberk2@gmail.com>
2022-02-23 21:58:24 +0300

    files updated

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get("/",(req,res)=>{
+    res.send('MERHABA')
+    
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} da çalışmaya başladı.`);
+});
