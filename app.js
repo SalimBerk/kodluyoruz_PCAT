12d01d87d7ccd47e2ca3f999e9599241c5c305ff
salim <salimberk2@gmail.com>
2022-03-03 00:32:56 +0300

    03.03.2022

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageControllers');
 const app = express();
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.set('view engine', 'ejs');
 
+app.use(fileUpload());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
-app.get('/', (req, res) => {
-  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  res.render('index');
-});
-app.get('/about', (req, res) => {
-  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  res.render('about');
-});
-app.get('/add', (req, res) => {
-  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  res.render('add');
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.post('/photos', photoController.createPhoto);
+app.get('/photos/edit/:id', pageController.getEditPage);
+app.put('/photos/:id', photoController.photoUpdate);
+app.delete('/photos/:id', photoController.photoDelete);
 
 const port = 3000;
 app.listen(port, () => {
